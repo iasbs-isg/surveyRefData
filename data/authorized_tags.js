bc59be953395f9d99ad082e0c54a7d0807d457a9
Amir Reza <amirreza.m95@gmail.com>
2022-01-17 22:48:38 +0330

    initial commit

---
+++ data/authorized_tags.js
@@ -0,0 +1,26 @@
+define({
+	authorizedTags: {
+		"architecture": {
+			"description": "type of the paper"
+		},
+		"application": {
+			"description": "type of the paper"
+		},
+		"adaptability": {
+			"description": "type of the paper"
+		},
+		"Experiments data": {
+			"description": "type of the paper"
+		},
+		"dataset": {
+			"description": "type of the paper"
+		},
+		"concentration": {
+			"description": "type of the paper"
+		},
+		"Main topic": {
+			"description": "type of the paper"
+		}
+		
+	}
+});
---
+++ data/tag_categories.js
@@ -0,0 +1,25 @@
+define({
+	tagCategories: {
+		"architecture": {
+			"description": "type of the paper"
+		},
+		"application": {
+			"description": "type of the paper"
+		},
+		"adaptability": {
+			"description": "type of the paper"
+		},
+		"Experiments data": {
+			"description": "type of the paper"
+		},
+		"dataset": {
+			"description": "type of the paper"
+		},
+		"concentration": {
+			"description": "type of the paper"
+		},
+		"Main topic": {
+			"description": "type of the paper"
+		}
+	}
+});
